b30c3c4e0f5dbacbba45ae5cc959a15ee34a8358
puneetbhatia77 <puneetbhatia77@gmail.com>
2019-01-09 14:01:30 +0530

    Update SampleResource.java

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -8,7 +8,7 @@ public class SampleResource {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "Welcome to Devops pipeline demo with docker !!";
     }
 
 }
